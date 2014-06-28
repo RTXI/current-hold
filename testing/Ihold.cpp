859665fbd3b5688cfa24cdfc7798b832dc2da91b
Ansel <aesthapus@gmail.com>
2014-06-28 17:09:12 -0400

    Added files to repo

---
+++ testing/Ihold.cpp
@@ -0,0 +1,80 @@
+/*
+ Copyright (C) 2011 Georgia Institute of Technology
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+/*
+ * This module simply outputs the specified current for as long as the module
+ * is active. It is designed to be programmatically paused and unpaused from
+ * another module (running an experimental module, for example) to implement
+ * a holding current in between trials.
+ */
+
+#include <Ihold.h>
+
+extern "C" Plugin::Object *
+createRTXIPlugin(void)
+{
+  return new Ihold();
+}
+
+static DefaultGUIModel::variable_t vars[] =
+  {
+    { "Command", "", DefaultGUIModel::OUTPUT, },
+    { "Holding Current (pA)", "Holding Current (pA)",
+        DefaultGUIModel::PARAMETER | DefaultGUIModel::DOUBLE, }, };
+
+static size_t num_vars = sizeof(vars) / sizeof(DefaultGUIModel::variable_t);
+
+Ihold::Ihold(void) :
+  DefaultGUIModel("Holding Current", ::vars, ::num_vars), current(-100e-12), on(
+      false)
+{
+
+  update( INIT);
+  refresh();
+}
+
+Ihold::~Ihold(void)
+{
+}
+
+void
+Ihold::execute(void)
+{
+  output(0) = current;
+}
+
+void
+Ihold::update(DefaultGUIModel::update_flags_t flag)
+{
+  switch (flag)
+    {
+  case INIT:
+    setParameter("Holding Current (pA)", QString::number(current * 1e12)); // convert from A to nA
+    break;
+  case MODIFY:
+    current = getParameter("Holding Current (pA)").toDouble() * 1e-12; // convert from pA to A
+    break;
+  case PAUSE:
+    output(0) = 0;
+  case PERIOD:
+  default:
+    break;
+    }
+
+}
+
